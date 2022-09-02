ca6fb7bea518e2bd55d26b4feeb960b39f5ef1b5
Julian Toscani <julian.toscani@rtl.com>
2022-09-02 23:48:53 +0200

    remove files, create more generic dataset

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
-import setData from "./setData";
+import setData from "./utils/setDotData";
 import "./style.css";
+import {select} from "d3";
 
-const createDataSet = () =>
+const createDataSet = (width = 600, height = 400, radius = 50) =>
     Array.from(new Array(4), () => {
         return {
-            cy: Math.floor(Math.random() * 300),
-            cx: Math.floor(Math.random() * 300),
-            r: Math.floor(Math.random() * 40),
+            cy:  Math.floor(Math.random() * (height - radius * 2)) + radius,
+            cx: Math.floor(Math.random() * (width - radius * 2)) + radius,
+            r: Math.floor(Math.random() * (radius - radius * 0.2)) + radius * 0.2,
         };
     });
 
+[1,2,3,4].forEach(()=>{ select('svg').append('circle')});
 const button = document.querySelector('button');
 
 button?.addEventListener('click', () => {
